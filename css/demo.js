10a01ed8f563185c45b18ad4893443647fc26103
Anjali1822 <anjalibhawari1810@gmail.com>
2023-04-16 17:24:28 +0530

    solve

---
+++ css/demo.js
@@ -0,0 +1,13 @@
+//let and const
+var c=5;
+function dis()
+{
+    var c=6;
+}
+var t=dis();
+console.log(t);
+
+ ag=18;
+nam="Anjali";
+ str=`Hi my name is ${nam} and I am ${ag} years old`;
+ console.log(str);
