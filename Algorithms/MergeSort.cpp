f71c678c93ff1e50bfd7ecfb89446eaa1b10df68
Anjali1822 <anjalibhawari1810@gmail.com>
2023-04-23 21:49:17 +0530

    solved

---
+++ Algorithms/MergeSort.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+        int i=l;
+        int j=m+1;
+
+        vector<int>temp;
+        while(i<=m && j<=r ){
+            if(arr[i]<arr[j]){
+              
+                temp.push_back(arr[i]);
+                i++;
+            }
+            else{
+               
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+       
+            while(j<=r){
+               temp.push_back(arr[j]);
+                j++;
+            }
+            while(i<=m){
+               temp.push_back(arr[i]);
+                i++;
+            }
+            for(int k=l;k<=r;k++){
+                arr[k]=temp[k-l];
+            }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        if(l>=r){
+            return;
+        }
+        int mid=l+(r-l)/2;
+       
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+};
